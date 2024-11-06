0fd524c2d12e73a087d1bf09b9abf90c07040601
Sam <samuel.grulich@gmail.com>
2024-11-06 17:34:16 +0100

    rewriting history

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,6 @@
-#include <cstdio>
-#include <chrono>
+#include <iostream>
 #include "lib.h"
 
 int main() {
-    auto start = std::chrono::high_resolution_clock::now();
-    int rows, cols;
-    scanf("%d %d", &rows, &cols);
-    State state(rows, cols);
-    auto initialization = std::chrono::high_resolution_clock::now();
-
-    for (int y = 0; y < rows; y++) {
-        for (int x = 0; x < cols; x++) {
-            uint value;
-            scanf("%d", &value);
-            state.parseInput(y, x, value);
-        }
-    }
-    auto input = std::chrono::high_resolution_clock::now();
-
-    uint sectorCount = state.sectorPrefixes(rows-1, cols-1);
-    uint sectorHalf = sectorCount / 2;
-    uint maxSum = 0;
-    uint maxStartX = 0, maxStartY = 0, maxEndX = 0, maxEndY = 0;
-    for (int startY = 0; startY < rows; startY++) {
-        for (int startX = 0; startX < cols; startX++) {
-            // todo add sector fields sqrt skipping
-            for (int endY = startY; endY < rows; endY++) {
-                for (int endX = startX; endX < cols; endX++) {
-                    // idea: check if the corners are sector values (if so, skip)
-                    //  1. check if it contains half of all sectors
-                    if (!state.containsHalfOfAllSectors(startX, startY, endX, endY, sectorHalf)) {
-                        continue;
-                    }
-                    //  2. check if there is a sector on any of the edges
-                    if (state.containsAnyEdgeSector(startX, startY, endX, endY)) {
-                        continue;
-                    }
-                    //  3. generate the sum of the inner rectangle
-                    uint sum = state.getBorderSum(startX, startY, endX, endY);
-                    if (sum > maxSum) {
-                        maxSum = sum;
-                        maxStartX = startX;
-                        maxStartY = startY;
-                        maxEndX = endX;
-                        maxEndY = endY;
-                    }
-                }
-            }
-        }
-    }
-    auto end = std::chrono::high_resolution_clock::now();
-
-    fprintf(stderr, "%d %d %d %d (%d)\n", maxStartY, maxStartX, maxEndY, maxEndX, maxSum);
-    fprintf(stderr, "Initialization: %ld\n", std::chrono::duration_cast<std::chrono::microseconds>(initialization - start).count());
-    fprintf(stderr, "Input: %ld\n", std::chrono::duration_cast<std::chrono::microseconds>(input - initialization).count());
-    fprintf(stderr, "End: %ld\n", std::chrono::duration_cast<std::chrono::microseconds>(end - input).count());
-    fprintf(stderr, "Total: %ld\n", std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
-    fprintf(stderr, "Loop count: %d\n", rows * cols * rows * cols);
-    printf("%d\n", maxSum);
-
-    return 0;
+    std::cout << "hello alg" << std::endl;
 }
-
--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Compiling..."
 cmake -B build -S .
 cmake --build build
 
@@ -14,16 +15,57 @@ if [ ! -d "tests/out" ]; then
     mkdir tests/out
 fi
 
+# parse options
+VERBOSE="f"
+getopts "v" opt; 
+case "$opt" in
+    v) VERBOSE="t"
+    ;;
+    *) echo "unknown argument passed ${OPTARG}"
+    ;;
+esac
+
+
 # run the program for each in file in tests/data
 input_files=$(ls tests/data | grep .in)
+tests_passed=()
+tests_failed=()
+tests=0
+echo
+echo "Running tests..."
 for file in $input_files; do
-    filename="${file}"
+    filename="${file%.in}" # remove the extension
+    # generate out files
     ./$filepath < tests/data/$file > tests/out/$filename.out 2> tests/out/$filename.err
-    echo $file
-    cat tests/out/$filename.err
-    echo "----------------"
+    # test the outputs
+    error_output=$(diff tests/out/$filename.out tests/data/$filename.out)
+    ret=$?
+    if [ $ret -eq 0 ]; then
+        echo "$filename test passed"
+        tests_passed+=( ${file} )
+    else
+        echo "$filename test failed"
+        tests_failed+=( ${file} )
+        echo 
+        echo "Diff output: "
+        printf '%s\n' ${error_output}
+        echo 
+    fi
+    if [ $VERBOSE = "t" -o $ret -ne 0 ]; then
+        echo "Program output: "
+        cat tests/out/$filename.err
+        echo "---------------------------------"
+    fi
+
+    let tests++;
 done
 
-# run diff tests/out with tests/data for each .out file
-output_files=$(ls tests/data | grep .out)
-my_output_files=$(ls tests/out | grep .out)
+echo
+echo "Tests summary: "
+echo "${#tests_passed[*]}/${tests} passed"
+if [ ${#tests_failed[*]} -eq 0 ]; then
+    echo "All tests passed"
+else
+    echo "Tests failed: ${tests_failed[*]}"
+fi
+
